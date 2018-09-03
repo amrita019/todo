2a847151e3328f78f85d43dc1aaae5bde6fcd22c
Amrita <amritachaturvedi97@gmail.com>
2018-09-04 00:16:26 +0530

    List Item created

--- app/src/main/java/com/amrita/to_do/MainActivity.java
+++ app/src/main/java/com/amrita/to_do/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -33,7 +34,7 @@ public class MainActivity extends AppCompatActivity {
         for (int i = 1; i <= 10; i++) {
             data.add(("Person " + i));
         }
-
+        Log.d("Debug", String.valueOf(data));
     }
 
     @Override
